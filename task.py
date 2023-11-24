26086762ceefd7c53a8be1fee736c33043409c7e
Aakash Thatte <aathatte2002@gmail.com>
2023-11-24 18:36:47 +0530

    Add SQL code for add and list

--- task.py
+++ task.py
@@ -1,12 +1,14 @@
 from colorama import Fore, Back, Style
 class ToDoList:
-    def __init__(self) -> None:
+    def __init__(self, cursor, list_name) -> None:
         self.task_details = {} # task_id: task_name pair
         # self.task_completed = {} # When completed it remove task from task details add it to task completed
+        self.cursor = cursor
+        self.list_name = list_name
+        self.cursor.execute(f"CREATE TABLE {self.list_name} (name TEXT)")
 
-    def add_task(self, task):
-        key = len(self.task_details)
-        self.task_details[key+1] = task
+    def add_task(self, task_name):
+        self.cursor.execute(f"INSERT INTO {self.list_name} VALUES ('{task_name}')")
         print("Task added Successfully")
 
     def delete_task(self, task_id):
@@ -18,6 +20,7 @@ class ToDoList:
         print(Fore.YELLOW + "Task deleted Successfully")
 
     def list_task(self):
+        rows = self.cursor.execute(f"SELECT * FROM {self.list_name}").fetchall
         print(Fore.LIGHTGREEN_EX + "Task details")
         for i, task in self.task_details.items():
             print(Fore.GREEN + f"{i}. Title: {task}")
