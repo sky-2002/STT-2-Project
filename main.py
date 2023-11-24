8660930e50b2dd7d24327464bdc86b34b9b416c7
Amogh Joshi <amoghjoshi500@gmail.com>
2023-11-24 19:03:52 +0530

    add while loop

--- main.py
+++ main.py
@@ -3,3 +3,35 @@ import sqlite3
 connection = sqlite3.connect("todo.db")
 
 cursor = connection.cursor()
+
+todo = ToDoList()
+
+while True:
+    # print options for user
+    print(Fore.WHITE,"\nTodo List Menu:")
+    print(Fore.BLUE, "1. Add Task")
+    print(Fore.BLUE, "2. List Tasks")
+    print(Fore.BLUE, "3. Mark Task as Completed")
+    print(Fore.BLUE, "4. Quit")
+    
+    usr_input = int(input(Fore.RED + "Enter your choice:"))
+
+    if usr_input == 1:
+        # If input is 1, call the corressponding function
+        task_input = input(Fore.RED + "Enter the task details:")
+        todo.add_task(task_input)
+    
+    elif usr_input == 2:
+        # If input is 2, list all tasks in the list
+        todo.list_task()
+
+    elif usr_input == 3:
+        # If input is 3, first show all current tasks
+        # then take input and delete the required task
+        todo.list_task()
+        i = int(input(Fore.RED + "Enter task id:"))
+        todo.delete_task(i)
+
+    elif usr_input == 4:
+        # Break out of the code if input is 4
+        break
